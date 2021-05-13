50f413e13da43938d8712f79226aca15ce1f3542
Agusgal <agaldeman@yahoo.com.ar>
2021-05-12 22:43:45 -0300

    codigo casi listo

--- TP4/Ej1/ej1.py
+++ TP4/Ej1/ej1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def roku4(f, t0, tf, deltaT, x0):
+def ruku4(f, t0, tf, deltaT, x0):
 
     # cantidad de puntos
 
@@ -19,9 +19,9 @@ def roku4(f, t0, tf, deltaT, x0):
         f1 = f(tk[i], xk[i, :])
         f2 = f(tk[i] + deltaT/2, xk[i, :] + ((deltaT/2) * f1))
         f3 = f(tk[i] + deltaT/2, xk[i, :] + ((deltaT/2) * f2))
-        f4 = f(tk[i] + deltaT/2, xk[i, :] + deltaT * f3)
+        f4 = f(tk[i] + deltaT, xk[i, :] + deltaT * f3)
 
-        xk[i + 1, :] = xk[i, :] + ((f1 + 2 * f2 + 2 * f3 + f4)/6) * deltaT
+        xk[i + 1, :] = xk[i, :] + ((f1 + 2*f2 + 2*f3 + f4)/6) * deltaT
 
     return tk, xk
 
--- TP4/Ej1/test_roku4.py
+++ TP4/Ej1/test_roku4.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from Ej1.ej1 import roku4
+from Ej1.ej1 import ruku4
 
 # Defino ecuación conocida (circuito con capacitor y resistenci)
 def dx(time, x):
@@ -8,7 +8,7 @@ def dx(time, x):
     C = 1e-6
     W = 2.0 * np.pi * 1000
     A = 1.0
-    T = 5*2*np.pi/W
+    T = 5 * 2 * np.pi / W
 
     return ((A*np.cos(W*time)-x)/(R*C))
 
@@ -29,9 +29,9 @@ def testRoku4():
     x0 = np.zeros(1)
     W = 2.0 * np.pi * 1000
     T = 5 * 2 * np.pi / W
-    h = T/10000
+    h = T/100
 
-    t, xe = roku4(dx, 0, T, h, x0)
+    t, xe = ruku4(dx, 0, T, h, x0)
     xknown = known(t)
 
     fig, ax = plt.subplots()
@@ -41,13 +41,36 @@ def testRoku4():
     ax.legend()
     plt.show()
 
-
     fig, ax = plt.subplots()
     ax.plot(t, xe[:, 0] - xknown, label='Error')
     ax.legend()
     plt.show()
 
-    # Falta agregar el tema del error con delta/2, delta
+    # Falta agregar el tema lineal:
+    lastknown = known(T)
+
+
+    t2, xe2 = ruku4(dx, 0, T, h/2, x0)
+    xknown2 = known(t2)
+
+    error = abs(xe2[-1, 0] - lastknown)
+    error1 = abs(xe[:, 0] - xknown)
+    error2 = abs(xe2[:, 0] - xknown2)
+
+    #print(error1)
+    #print(error2)
+
+    plt.plot(t, error1, t2, error2)
+    plt.show()
+
+
+    estimacion = abs(xe[-1, 0] - xe2[-1, 0])/15
+    print(estimacion)
+    print(error)
+
+    porcentual = (abs(error - estimacion)/error*100)
+    print(porcentual)
+
 
 if __name__ == '__main__':
     testRoku4()
